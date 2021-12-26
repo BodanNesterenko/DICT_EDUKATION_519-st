0af45b9f282cf587ece20cf883ae62214fa9526e
bohdan <svoih.ne.brosim.1337@gmail.com>
2021-12-26 21:42:47 +0200

    TicTakToe 2 stage

---
+++ TicTacToe/TicTakToe.py
@@ -0,0 +1,17 @@
+mat = input("Enter cells:").split()
+for i in range(9):
+    mat[i] = mat[i]
+
+playboard = [["", "", ""],
+             ["", "", ""],
+             ["", "", ""]]
+
+print("""
+------------
+| {} {} {} |
+| {} {} {} |
+| {} {} {} |
+------------
+""".format(mat[0], mat[1], mat[2],
+           mat[3], mat[4], mat[5],
+           mat[6], mat[7], mat[8]))
