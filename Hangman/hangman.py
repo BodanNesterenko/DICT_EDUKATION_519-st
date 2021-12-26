16a8c2b78d026250444efdaf78dabcef0532ec4a
bohdan <svoih.ne.brosim.1337@gmail.com>
2021-12-26 20:57:20 +0200

    Hangman 3 stage

---
+++ Hangman/hangman.py
@@ -0,0 +1,34 @@
+import random
+word_list = ["Space",
+             "Human",
+             "Cat",
+             "Cow",
+             "Rocket",
+             "Star"]
+
+
+def announce():
+    print("HANGAMAN")
+    # print("The game will avalible soon")
+
+
+def word_selection():
+    word = random.choice(word_list)
+    return word.lower()
+
+
+def game(word):
+    guessed_word = input("Guess the word:>").lower()
+    if guessed_word == word:
+        print("You guess the word,", guessed_word, "\n You survived")
+    else:
+        print("You lose, try again")
+
+
+def main():
+    announce()
+    word = word_selection()
+    game(word)
+
+
+main()
